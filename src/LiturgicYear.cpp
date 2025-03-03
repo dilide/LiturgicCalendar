21899c09d29a2334754b98dc5fc63f5dcc11debe
liyake <me@liyake.com>
2025-03-03 10:59:08 +0800

    增加“董思高”

--- src/LiturgicYear.cpp
+++ src/LiturgicYear.cpp
@@ -635,6 +635,7 @@ void CathAssist::Calendar::LiturgicYear::initPropers()
         mapPropers.insert(std::make_pair(1110701,CellInfo(OPTIONAL,NOCOLOR,"圣威利巴(欧德莱主教 St. Willbrord 658-739)")));
         mapPropers.insert(std::make_pair(1110702,CellInfo(OPTIONAL,NOCOLOR,"真福安多尼•白田诺西(Bd. Antony Baldinucci)")));
         mapPropers.insert(std::make_pair(1110801,CellInfo(OPTIONAL,NOCOLOR,"圣高弗烈(亚弥央斯主教 St. Godfrey -1115)")));
+        mapPropers.insert(std::make_pair(1110802,CellInfo(OPTIONAL,NOCOLOR,"真福若望·董思高(司铎圣师 St. John Duns Scott 1266—1308)")));
         mapPropers.insert(std::make_pair(1110901,CellInfo(LORD,WHITE,"拉特朗大殿奉献日(The Dedication of the Lateran Basilica)")));
         mapPropers.insert(std::make_pair(1111001,CellInfo(MEMORIAL,WHITE,"圣良一世(主教 St. Leo the Great)")));
         mapPropers.insert(std::make_pair(1111002,CellInfo(OPTIONAL,NOCOLOR,"圣安德肋•亚委琳(St. Andrew Avellino)")));
